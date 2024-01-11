d17be832dafe688f4c8981baef44362b99d295b7
Yaroslavskiba322 <yaroslavskiba322@gmail.com>
2024-01-11 22:18:54 +0300

    ...

--- src/components/paintings/Painting.component.tsx
+++ src/components/paintings/Painting.component.tsx
@@ -2,44 +2,48 @@ import { Painting } from '@/api/api';
 import Image from 'next/image';
 import styles from '../component.style.module.css';
 import MDivScroll from '../motionComponents/MotionDivScroll';
+import { Suspense } from 'react';
+import Loading from '@/app/loading';
 
 function PaintingComponent({ url, description, id, name, cost }: Painting) {
   return (
     <>
-      <article style={{ display: 'contents' }}>
-        <MDivScroll name={name}>
-          <section className={styles.painting}>
-            <Image
-              className={styles.imageSize}
-              src={url}
-              width={720}
-              height={720}
-              alt={name}
-            />
-            <div className={styles.description}>
-              <p>
-                Номер картины: <b>{id}</b>
-              </p>
-              <br />
-              <h2>{name}</h2>
-              <br />
-              <p>{description}</p>
-              <br />
-              <p>
-                <i>${cost}</i>
-              </p>
-              <br />
-              <a
-                className={styles.textLinkImage}
-                href='https://t.me/Ev_Cath_helper_bot'
-                target='blank'
-              >
-                <i>Задать вопрос или Заказать</i>
-              </a>
-            </div>
-          </section>
-        </MDivScroll>
-      </article>
+      <Suspense fallback={<Loading />}>
+        <article style={{ display: 'contents' }}>
+          <MDivScroll name={name}>
+            <section className={styles.painting}>
+              <Image
+                className={styles.imageSize}
+                src={url}
+                width={720}
+                height={720}
+                alt={name}
+              />
+              <div className={styles.description}>
+                <p>
+                  Номер картины: <b>{id}</b>
+                </p>
+                <br />
+                <h2>{name}</h2>
+                <br />
+                <p>{description}</p>
+                <br />
+                <p>
+                  <i>${cost}</i>
+                </p>
+                <br />
+                <a
+                  className={styles.textLinkImage}
+                  href='https://t.me/Ev_Cath_helper_bot'
+                  target='blank'
+                >
+                  <i>Вопросы & оформление</i>
+                </a>
+              </div>
+            </section>
+          </MDivScroll>
+        </article>
+      </Suspense>
     </>
   );
 }
--- src/components/walls/Wall.component.tsx
+++ src/components/walls/Wall.component.tsx
@@ -3,42 +3,45 @@ import Image from 'next/image';
 import styles from '../component.style.module.css';
 import MDivScroll from '../motionComponents/MotionDivScroll';
 import Loading from '@/app/loading';
+import { Suspense } from 'react';
 
 function WallComponent({ url, id, type, cost }: Walls) {
   return (
     <>
-      <article style={{ display: 'contents' }}>
-        <MDivScroll name={type}>
-          <section className={styles.painting}>
-            <Image
-              className={styles.imageSize}
-              src={url}
-              width={720}
-              height={720}
-              alt={type}
-            />
-            <div className={styles.description}>
-              <p>
-                Номер картины: <b>{id}</b>
-              </p>
-              <br />
-              <h2>{type}</h2>
-              <br />
-              <p>
-                <i>${cost}</i>
-              </p>
-              <br />
-              <a
-                className={styles.textLinkImage}
-                href='https://t.me/Ev_Cath_helper_bot'
-                target='blank'
-              >
-                <i>Задать вопрос или Заказать</i>
-              </a>
-            </div>
-          </section>
-        </MDivScroll>
-      </article>
+      <Suspense fallback={<Loading />}>
+        <article style={{ display: 'contents' }}>
+          <MDivScroll name={type}>
+            <section className={styles.painting}>
+              <Image
+                className={styles.imageSize}
+                src={url}
+                width={720}
+                height={720}
+                alt={type}
+              />
+              <div className={styles.description}>
+                <p>
+                  Номер картины: <b>{id}</b>
+                </p>
+                <br />
+                <h2>{type}</h2>
+                <br />
+                <p>
+                  <i>${cost}</i>
+                </p>
+                <br />
+                <a
+                  className={styles.textLinkImage}
+                  href='https://t.me/Ev_Cath_helper_bot'
+                  target='blank'
+                >
+                  <i>Вопросы & оформление</i>
+                </a>
+              </div>
+            </section>
+          </MDivScroll>
+        </article>
+      </Suspense>
     </>
   );
 }
