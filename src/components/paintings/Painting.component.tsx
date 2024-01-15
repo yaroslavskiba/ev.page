28c8a4db23e78472091382df964657fe9a6d7c3e
Yaroslavskiba322 <yaroslavskiba322@gmail.com>
2024-01-15 19:06:28 +0300

    ?

--- src/components/paintings/Painting.component.tsx
+++ src/components/paintings/Painting.component.tsx
@@ -6,43 +6,62 @@ import styles from '../component.style.module.css';
 import MDivScroll from '../motionComponents/MotionDivScroll';
 import Link from 'next/link';
 import MInfinityDiv from '../motionComponents/MInfinityDiv';
+import { useEffect, useState } from 'react';
+import Loading from '@/app/loading';
 
 function PaintingComponent({ url, description, id, name, cost }: Painting) {
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => handleLoad(), 700);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  const handleLoad = () => {
+    setIsLoad(true);
+  };
+
   return (
     <>
       <article style={{ display: 'contents' }}>
         <MDivScroll name={name}>
-          <section className={styles.painting}>
-            <Image
-              className={styles.imageSize}
-              src={url}
-              width={768}
-              height={768}
-              alt={name}
-            />
-            <MInfinityDiv />
-            <div className={styles.description}>
-              <p>
-                Номер картины: <b>{id}</b>
-              </p>
-              <br />
-              <h2>{name}</h2>
-              <br />
-              <p>{description}</p>
-              <br />
-              <p>
-                <i>${cost}</i>
-              </p>
-              <br />
-              <Link
-                href={`/gallery/paintings/${id}`}
-                className={styles.textLinkImage}
-                style={{ padding: '10px 15px' }}
-              >
-                <i>Подробнее</i>
-              </Link>
-            </div>
-          </section>
+          {isLoad ? (
+            <section className={styles.painting}>
+              <Image
+                className={styles.imageSize}
+                src={url}
+                width={768}
+                height={768}
+                alt={name}
+              />
+              <MInfinityDiv />
+              <div className={styles.description}>
+                <p>
+                  Номер картины: <b>{id}</b>
+                </p>
+                <br />
+                <h2>{name}</h2>
+                <br />
+                <p>{description}</p>
+                <br />
+                <p>
+                  <i>${cost}</i>
+                </p>
+                <br />
+                <Link
+                  href={`/gallery/paintings/${id}`}
+                  className={styles.textLinkImage}
+                  style={{ padding: '10px 15px' }}
+                >
+                  <i>Подробнее</i>
+                </Link>
+              </div>
+            </section>
+          ) : (
+            <Loading />
+          )}
         </MDivScroll>
       </article>
     </>
--- src/components/walls/Wall.component.tsx
+++ src/components/walls/Wall.component.tsx
@@ -1,43 +1,65 @@
+'use client';
+
 import { Walls } from '@/api/api';
 import Image from 'next/image';
 import styles from '../component.style.module.css';
 import MDivScroll from '../motionComponents/MotionDivScroll';
 import Link from 'next/link';
+import MInfinityDiv from '../motionComponents/MInfinityDiv';
+import { useEffect, useState } from 'react';
+import Loading from '@/app/loading';
 
 function WallComponent({ url, id, type, cost }: Walls) {
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => handleLoad(), 700);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  const handleLoad = () => {
+    setIsLoad(true);
+  };
+
   return (
     <>
       <article style={{ display: 'contents' }}>
         <MDivScroll name={type}>
-          <section className={styles.painting}>
-            <Image
-              className={styles.imageSize}
-              src={url}
-              width={768}
-              height={768}
-              alt={type}
-            />
-
-            <div className={styles.description}>
-              <p>
-                Номер декора: <b>{id}</b>
-              </p>
-              <br />
-              <h2>{type}</h2>
-              <br />
-              <p>
-                <i>${cost}</i>
-              </p>
-              <br />
-              <Link
-                href={`/gallery/walls/${id}`}
-                className={styles.textLinkImage}
-                style={{ padding: '10px 15px' }}
-              >
-                <i>Подробнее</i>
-              </Link>
-            </div>
-          </section>
+          {isLoad ? (
+            <section className={styles.painting}>
+              <Image
+                className={styles.imageSize}
+                src={url}
+                width={768}
+                height={768}
+                alt={type}
+              />
+              <MInfinityDiv />
+              <div className={styles.description}>
+                <p>
+                  Номер декора: <b>{id}</b>
+                </p>
+                <br />
+                <h2>{type}</h2>
+                <br />
+                <p>
+                  <i>${cost}</i>
+                </p>
+                <br />
+                <Link
+                  href={`/gallery/walls/${id}`}
+                  className={styles.textLinkImage}
+                  style={{ padding: '10px 15px' }}
+                >
+                  <i>Подробнее</i>
+                </Link>
+              </div>
+            </section>
+          ) : (
+            <Loading />
+          )}
         </MDivScroll>
       </article>
     </>
