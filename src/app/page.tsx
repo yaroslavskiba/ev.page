f64b96337c028306c4948f7628598fbad8d86b00
Yaroslavskiba322 <yaroslavskiba322@gmail.com>
2024-01-29 22:04:56 +0300

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,16 +29,6 @@ function Page() {
               <MTextScroll custom={3}>
                 <b>-</b> <i>художник / декоратор стен</i>
               </MTextScroll>
-              <br />
-              <MGroupDiv custom={4}>
-                <Link className='link-button' href={'/my-works/painitngs'}>
-                  Картины
-                </Link>
-                <Link className='link-button' href={'/my-works/walls'}>
-                  Декор стен
-                </Link>
-              </MGroupDiv>
-              <br />
             </>
           </div>
         </div>
@@ -60,9 +50,10 @@ function Page() {
             </MTitleScroll>
           </div>
           <div className='media'>
-            <MDivScrollContainer>
-              <Video src='/v_2.mp4' />
-            </MDivScrollContainer>
+            <CustomImage
+              url={'/staticImages/main1.webp'}
+              alt={'Главное изображение'}
+            />
           </div>
         </div>
       </div>
@@ -71,40 +62,3 @@ function Page() {
 }
 
 export default Page;
-
-{
-  /* <section className='info-block'>
-        <MDivImage custom={1}>
-          <CustomImage url='/staticImages/main1.webp' alt='main image' />
-        </MDivImage>
-        <div className='split'>
-          <MHeader custom={2}>Екатерина Евсеенко</MHeader>
-          <MParagraph custom={2}>
-            Привет! Меня зовут <b>Екатерина Евсеенко</b>, и я художница и
-            декоратор стен. Мне 25 лет, и я родилась в прекрасном городе
-            Витебске. Страсть к искусству и рисованию меня сопровождала с
-            детства, и поэтому я решила посвятить свою жизнь творчеству.
-          </MParagraph>
-          <MParagraph custom={3}>
-            Сейчас я живу и работаю в Минске, где мне удалось найти прекрасные
-            возможности для развития в области искусства. Здесь я нашла много
-            интересных проектов и сотрудничеств, которые помогают мне раскрыть
-            свой творческий потенциал.
-          </MParagraph>
-          <MParagraph custom={4}>
-            Мое вдохновение находится в природе, и я обожаю рисовать объемные
-            картины, которые передают красоту окружающего мира.
-          </MParagraph>
-          <MParagraph custom={5}>
-            Я счастлива, что моя работа позволяет мне делиться своим творчеством
-            и вдохновлять других людей. Будь то украшение интерьера, создание
-            уникальной атмосферы или просто наслаждение красотой искусства, я
-            надеюсь, что мои работы оставят яркий след в сердцах людей.
-          </MParagraph>
-          <MParagraph custom={6}>
-            Я всегда открыта для новых проектов и с нетерпением жду возможности
-            поделиться своим искусством с вами.
-          </MParagraph>
-        </div>
-      </section> */
-}
