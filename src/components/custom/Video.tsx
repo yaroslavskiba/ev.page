8674644353c91a3f91804b19635447672e280df9
Yaroslavskiba322 <yaroslavskiba322@gmail.com>
2024-01-30 17:31:09 +0300

    ...

--- src/components/custom/Video.tsx
+++ src/components/custom/Video.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, Suspense } from 'react';
 import styles from './video.styles.module.css';
 import { FaPlay, FaPause, FaRedo } from 'react-icons/fa';
+import Loading from '@/app/loading';
 
 type VideoType = {
   src: string;
@@ -54,39 +55,41 @@ export function Video({ src }: VideoType) {
   }, []);
 
   return (
-    <div className={styles.player}>
-      <video
-        ref={ref}
-        className={styles.videoContainer}
-        preload='auto'
-        playsInline
-        muted
-        controls={false}
-      >
-        <source src={src} type='video/mp4' />
-        Your browser does not support the video tag.
-      </video>
-      <div className={styles.buttons}>
-        {!hasEnded && (
-          <>
-            {isPlaying ? (
-              <button className={styles.hidden} onClick={handlePause}>
-                Кнопка
-              </button>
-            ) : (
-              <button className={styles.playerButton} onClick={handlePlay}>
-                <FaPlay />
-              </button>
-            )}
-          </>
-        )}
-        {hasEnded && (
-          <button className={styles.playerButton} onClick={handleReplay}>
-            <FaRedo />
-          </button>
-        )}
+    <Suspense fallback={<Loading />}>
+      <div className={styles.player}>
+        <video
+          ref={ref}
+          className={styles.videoContainer}
+          preload='auto'
+          playsInline
+          muted
+          controls={false}
+        >
+          <source src={src} type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+        <div className={styles.buttons}>
+          {!hasEnded && (
+            <>
+              {isPlaying ? (
+                <button className={styles.hidden} onClick={handlePause}>
+                  Кнопка
+                </button>
+              ) : (
+                <button className={styles.playerButton} onClick={handlePlay}>
+                  <FaPlay />
+                </button>
+              )}
+            </>
+          )}
+          {hasEnded && (
+            <button className={styles.playerButton} onClick={handleReplay}>
+              <FaRedo />
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
