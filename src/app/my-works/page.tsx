1730218c4784f463afa9b9baba464a5f00e4a8d1
Yaroslavskiba322 <yaroslavskiba322@gmail.com>
2024-02-01 22:02:04 +0300

    ?

--- src/app/my-works/page.tsx
+++ src/app/my-works/page.tsx
@@ -1,5 +1,6 @@
 import NavHeader from '@/components/custom/NavHeader';
 import { MTitleScroll } from '@/components/motionComponents/MMainComponents';
+import Link from 'next/link';
 
 function Page() {
   return (
@@ -8,6 +9,12 @@ function Page() {
       <div className='info-block'></div>
       <MTitleScroll custom={1}>
         Будет переделываться, мне не нравится
+        <Link href={'/my-works/paintings'} as={'/my-works/paintings'}>
+          Картины
+        </Link>
+        <Link href={'/my-works/walls'} as={'/my-works/walls'}>
+          Стены
+        </Link>
       </MTitleScroll>
     </div>
   );
